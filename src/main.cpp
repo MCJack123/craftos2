4520d3963f5d10f37b7659f184b1ad0c650ea1fc
MCJack123 <jackbruienne@yahoo.com>
2019-11-07 00:32:19 -0500

    Fixed update check branch

--- src/main.cpp
+++ src/main.cpp
@@ -40,53 +40,57 @@ std::string updateAtQuit;
 void update_thread() {
     try {
         Poco::Net::HTTPSClientSession session("api.github.com", 443, new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", Poco::Net::Context::VERIFY_NONE, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH"));
-        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, "/repos/MCJack123/craftos2/releases/latest", Poco::Net::HTTPMessage::HTTP_1_1);
+        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, "/repos/MCJack123/craftos2/releases", Poco::Net::HTTPMessage::HTTP_1_1);
         Poco::Net::HTTPResponse response;
         session.setTimeout(Poco::Timespan(5000000));
         request.add("User-Agent", "CraftOS-PC/2.0 Poco/1.9.3");
         session.sendRequest(request);
         Poco::JSON::Parser parser;
         parser.parse(session.receiveResponse(response));
-        Poco::JSON::Object::Ptr root = parser.asVar().extract<Poco::JSON::Object::Ptr>();
-        if (root->getValue<std::string>("tag_name") != CRAFTOSPC_VERSION) {
+        Poco::JSON::Array::Ptr root = parser.asVar().extract<Poco::JSON::Array::Ptr>();
+        for (auto it = root->begin(); it != root->end(); it++) {
+            Poco::JSON::Object::Ptr obj = it->extract<Poco::JSON::Object::Ptr>();
+            if (obj->getValue<std::string>("target_commitish") == "luajit" && obj->getValue<std::string>("tag_name") != CRAFTOSPC_VERSION) {
 #if defined(__APPLE__) || defined(WIN32)
-            SDL_MessageBoxData msg;
-            SDL_MessageBoxButtonData buttons[] = {
-                {0, 0, "Skip This Version"},
-                {SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Ask Me Later"},
-                {0, 2, "Update On Quit"},
-                {SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 3, "Update Now"}
-            };
-            msg.flags = SDL_MESSAGEBOX_INFORMATION;
-            msg.window = NULL;
-            msg.title = "Update available!";
-            std::string message = (std::string("A new update to CraftOS-PC is available (") + root->getValue<std::string>("tag_name") + ", you have " CRAFTOSPC_VERSION "). Would you like to update to the latest version?");
-            msg.message = message.c_str();
-            msg.numbuttons = 4;
-            msg.buttons = buttons;
-            msg.colorScheme = NULL;
-            int* choicep = (int*)queueTask([ ](void* arg)->void*{int* num = new int; SDL_ShowMessageBox((SDL_MessageBoxData*)arg, num); return num;}, &msg);
-            int choice = *choicep;
-            delete choicep;
-            switch (choice) {
-                case 0:
-                    config.skipUpdate = CRAFTOSPC_VERSION;
-                    return;
-                case 1:
-                    return;
-                case 2:
-                    updateAtQuit = root->getValue<std::string>("tag_name");
-                    return;
-                case 3:
-                    queueTask([root](void*)->void*{updateNow(root->getValue<std::string>("tag_name")); return NULL;}, NULL);
-                    return;
-                default:
-                    // this should never happen
-                    exit(choice);
-            }
+                SDL_MessageBoxData msg;
+                SDL_MessageBoxButtonData buttons[] = {
+                    {0, 0, "Skip This Version"},
+                    {SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Ask Me Later"},
+                    {0, 2, "Update On Quit"},
+                    {SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 3, "Update Now"}
+                };
+                msg.flags = SDL_MESSAGEBOX_INFORMATION;
+                msg.window = NULL;
+                msg.title = "Update available!";
+                std::string message = (std::string("A new update to CraftOS-PC is available (") + obj->getValue<std::string>("tag_name") + ", you have " CRAFTOSPC_VERSION "). Would you like to update to the latest version?");
+                msg.message = message.c_str();
+                msg.numbuttons = 4;
+                msg.buttons = buttons;
+                msg.colorScheme = NULL;
+                int* choicep = (int*)queueTask([ ](void* arg)->void*{int* num = new int; SDL_ShowMessageBox((SDL_MessageBoxData*)arg, num); return num;}, &msg);
+                int choice = *choicep;
+                delete choicep;
+                switch (choice) {
+                    case 0:
+                        config.skipUpdate = CRAFTOSPC_VERSION;
+                        return;
+                    case 1:
+                        return;
+                    case 2:
+                        updateAtQuit = obj->getValue<std::string>("tag_name");
+                        return;
+                    case 3:
+                        queueTask([obj](void*)->void*{updateNow(obj->getValue<std::string>("tag_name")); return NULL;}, NULL);
+                        return;
+                    default:
+                        // this should never happen
+                        exit(choice);
+                }
 #else
-            queueTask([](void* arg)->void* {SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Update available!", (const char*)arg, NULL); return NULL; }, (void*)(std::string("A new update to CraftOS-PC is available (") + root->getValue<std::string>("tag_name") + ", you have " CRAFTOSPC_VERSION "). Go to " + root->getValue<std::string>("html_url") + " to download the new version.").c_str());
+                queueTask([](void* arg)->void* {SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Update available!", (const char*)arg, NULL); return NULL; }, (void*)(std::string("A new update to CraftOS-PC is available (") + obj->getValue<std::string>("tag_name") + ", you have " CRAFTOSPC_VERSION "). Go to " + obj->getValue<std::string>("html_url") + " to download the new version.").c_str());
+                return;
 #endif
+            }
         }
     } catch (std::exception &e) {
         printf("Could not check for updates: %s\n", e.what());
