c96507a9ec469714e71a18669978d3ac7f36c9b1
MCJack123 <jackbruienne@yahoo.com>
2019-11-07 22:54:08 -0500

    Updated icons

--- src/favicon.h
+++ src/favicon.h
@@ -1,7 +1,7 @@
 /*  GIMP header image file format (RGB): src/favicon.h  */
 
-static unsigned int favicon_width = 16;
-static unsigned int favicon_height = 16;
+static unsigned int width = 16;
+static unsigned int height = 16;
 
 /*  Call this macro repeatedly.  After each use, the pixel data can be extracted  */
 
@@ -12,20 +12,20 @@ pixel[2] = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33))); \
 data += 4; \
 }
 static const char *header_data =
-	".T=X.$1U.$1U-D)S,S]P,CYO,#QM,#QM)S-D/DI[.T=X.$1U,#QM*C9G.45V-4%R"
-	".T=XA)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!A)#!-4%R"
-	"+#AIA)#!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!,S]P"
-	",#QMA)#!*C9G`P\\_*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!+#AI"
-	".T=XA)#!*C9G*C9G`P\\_*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!)S-D"
-	"+CIKA)#!*C9G`P\\_*C9G*C9G`P\\_`P\\_*C9G*C9G*C9G*C9G*C9G*C9GA)#!)S-D"
-	"-4%RA)#!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!)C)C"
-	",CYOA)#!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!/4EZ"
-	",CYOA)#!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GA)#!/DI["
-	"3EJ+Q-$!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GQ-$!0DY_"
-	"3EJ+Q-$!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GQ-$!,S]P"
-	"3EJ+Q-$!*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9G*C9GQ-$!-4%R"
-	"3EJ+Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!,#QM"
-	"35F*Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!*C9G*C9GQ-$!-4%R"
-	"4%R-Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q-$!,#QM"
-	"0DY_-4%R-4%R,S]P,S]P,S]P,S]P,S]P,S]P,#QM,S]P,S]P,S]P,S]P-4%R-4%R"
+	"/$AY.45V.45V-T-T-$!Q,S]P,3UN,3UN*#1E/TM\\/$AY.45V,3UN*S=H.D9W-D)S"
+	"/$AYT];(TM7%T]?$T]K\"T]S\"T]S\"T]S\"T]K\"T]G\"T]O\"U-O!U-O!U-F_U-N]-D)S"
+	"+3EJT]W&%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2T=JT-$!Q"
+	",3UNU-_!%2%2````%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2T]RX+3EJ"
+	"/$AYU-_!%2%2%2%2````%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2T]JX*#1E"
+	"+SMLTM_\"%2%2````%2%2%2%2````````%2%2%2%2%2%2%2%2%2%2%2%2TMV[*#1E"
+	"-D)ST=[#%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2T]JX)S-D"
+	",S]PT=['%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2TMFU/DI["
+	",S]PU-_'%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2S]2R/TM\\"
+	"3UN,YN_=%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2Y>W00T]`"
+	"3UN,Y>_@%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2Y>K.-$!Q"
+	"3UN,YO'C%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2X^'(-D)S"
+	"3UN,Y?'@Y?#@Y?#@Y?'@Y?'@Y?+?YN_=Y>[<Y>[:YNO3Y>C.X>#(X>#&XM[%,3UN"
+	"3EJ+Y?':X_#7X_#7Y/':Y?':Y?':YO'9Y>[6Y.W5Y.?-Y.+'*S=H*S=HY.#%-D)S"
+	"45V.YO'3Y?#2Y>[0Y>[2YN_1YNW3YNW3YNO3Y>K0Y./)Y.#%XMO!X=J`XMB_,3UN"
+	"0T]`-D)S-D)S-$!Q-$!Q-$!Q-$!Q-$!Q-$!Q,3UN-$!Q-$!Q-$!Q-$!Q-D)S-D)S"
 	"";
