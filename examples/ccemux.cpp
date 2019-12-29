04b5b0e595875714e72ffc944aa4058cadb2e2ff
MCJack123 <jackmacwindowslinux@gmail.com>
2019-12-29 00:56:53 -0500

    v2.2.1-luajit: Fixed plugin

--- examples/ccemux.cpp
+++ examples/ccemux.cpp
@@ -25,7 +25,7 @@ extern "C" {
 #endif
 #define libFunc(lib, name) getLibraryFunction(getLibrary(lib), name)
 
-#define PLUGIN_VERSION 2
+#define PLUGIN_VERSION 3
 
 library_t * (*getLibrary)(std::string);
 
