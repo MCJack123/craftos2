6c3969c64e7fb0861cb28d2cf078ddf027886e50
MCJack123 <jackmacwindowslinux@gmail.com>
2020-03-20 23:23:29 -0400

    Fixed packStandaloneROM

--- packStandaloneROM.js
+++ packStandaloneROM.js
@@ -19,208 +19,208 @@ function writeDir(path, level) {
         fs.writeSync(out, "},\n")
     }
     if (path == "rom/apis") {
-        fs.writeSync(out, `${' '.repeat(level * 4)}{"${f}", "-- Definition for the IO API
-
-local g_defaultInput = {
-    bFileHandle = true,
-    bClosed = false,
-    close = function( self )
-    end,
-    read = function( self, _sFormat )
-        if _sFormat and _sFormat ~= "*l" then
-            error( "Unsupported format" )
-        end
-        return _G.read()
-    end,
-    lines = function( self )
-        return function()
-            return _G.read()
-        end
-    end,
-}
-
-local g_defaultOutput = {
-    bFileHandle = true,
-    bClosed = false,
-    close = function( self )
-    end,
-    write = function( self, ... )
-        local nLimit = select("#", ... )
-        for n = 1, nLimit do
-            _G.write( select( n, ... ) )
-        end
-    end,
-    flush = function( self )
-    end,
-}
-
-local g_currentInput = g_defaultInput
-local g_currentOutput = g_defaultOutput
-
-function close( _file )
-    (_file or g_currentOutput):close()
-end
-
-function flush()
-    g_currentOutput:flush()
-end
-
-function input( _arg )
-    if _G.type( _arg ) == "string" then
-        g_currentInput = open( _arg, "r" )
-    elseif _G.type( _arg ) == "table" then
-        g_currentInput = _arg
-    elseif _G.type( _arg ) == "nil" then
-        return g_currentInput
-    else
-        error( "bad argument #1 (expected string/table/nil, got " .. _G.type( _arg ) .. ")", 2 )
-    end
-end
-
-function lines( _sFileName )
-    if _G.type( _sFileName ) ~= "string" then
-        error( "bad argument #1 (expected string, got " .. _G.type( _sFileName ) .. ")", 2 )
-    end
-    if _sFileName then
-        return open( _sFileName, "r" ):lines()
-    else
-        return g_currentInput:lines()
-    end
-end
-
-function open( _sPath, _sMode )
-    if _G.type( _sPath ) ~= "string" then
-        error( "bad argument #1 (expected string, got " .. _G.type( _sPath ) .. ")", 2 )
-    end
-    if _sMode ~= nil and _G.type( _sMode ) ~= "string" then
-        error( "bad argument #2 (expected string, got " .. _G.type( _sMode ) .. ")", 2 )
-    end
-    local sMode = _sMode or "r"
-    local file, err = fs.open( _sPath, sMode )
-    if not file then
-        return nil, err
-    end
-    
-    if sMode == "r"then
-        return {
-            bFileHandle = true,
-            bClosed = false,				
-            close = function( self )
-                file.close()
-                self.bClosed = true
-            end,
-            read = function( self, _sFormat )
-                local sFormat = _sFormat or "*l"
-                if sFormat == "*l" then
-                    return file.readLine()
-                elseif sFormat == "*a" then
-                    return file.readAll()
-                elseif _G.type( sFormat ) == "number" then
-                    return file.read( sFormat )
-                else
-                    error( "Unsupported format", 2 )
-                end
-                return nil
-            end,
-            lines = function( self )
-                return function()
-                    local sLine = file.readLine()
-                    if sLine == nil then
-                        file.close()
-                        self.bClosed = true
-                    end
-                    return sLine
-                end
-            end,
-        }
-    elseif sMode == "w" or sMode == "a" then
-        return {
-            bFileHandle = true,
-            bClosed = false,				
-            close = function( self )
-                file.close()
-                self.bClosed = true
-            end,
-            write = function( self, ... )
-                local nLimit = select("#", ... )
-                for n = 1, nLimit do
-                    file.write( select( n, ... ) )
-                end
-            end,
-            flush = function( self )
-                file.flush()
-            end,
-        }
-    
-    elseif sMode == "rb" then
-        return {
-            bFileHandle = true,
-            bClosed = false,				
-            close = function( self )
-                file.close()
-                self.bClosed = true
-            end,
-            read = function( self )
-                return file.read()
-            end,
-        }
-        
-    elseif sMode == "wb" or sMode == "ab" then
-        return {
-            bFileHandle = true,
-            bClosed = false,				
-            close = function( self )
-                file.close()
-                self.bClosed = true
-            end,
-            write = function( self, ... )
-                local nLimit = select("#", ... )
-                for n = 1, nLimit do
-                    file.write( select( n, ... ) )
-                end
-            end,
-            flush = function( self )
-                file.flush()
-            end,
-        }
-    
-    else
-        file.close()
-        error( "Unsupported mode", 2 )
-        
-    end
-end
-
-function output( _arg )
-    if _G.type( _arg ) == "string" then
-        g_currentOutput = open( _arg, "w" )
-    elseif _G.type( _arg ) == "table" then
-        g_currentOutput = _arg
-    elseif _G.type( _arg ) == "nil" then
-        return g_currentOutput
-    else
-        error( "bad argument #1 (expected string/table/nil, got " .. _G.type( _arg ) .. ")", 2 )
-    end
-end
-
-function read( ... )
-    return input():read( ... )
-end
-
-function type( _handle )
-    if _G.type( _handle ) == "table" and _handle.bFileHandle == true then
-        if _handle.bClosed then
-            return "closed file"
-        else
-            return "file"
-        end
-    end
-    return nil
-end
-
-function write( ... )
-    return output():write( ... )
-end
+        fs.writeSync(out, `${' '.repeat(level * 4)}{"${f}", "-- Definition for the IO API\\n\\
+\\n\\
+local g_defaultInput = {\\n\\
+    bFileHandle = true,\\n\\
+    bClosed = false,\\n\\
+    close = function( self )\\n\\
+    end,\\n\\
+    read = function( self, _sFormat )\\n\\
+        if _sFormat and _sFormat ~= "*l" then\\n\\
+            error( "Unsupported format" )\\n\\
+        end\\n\\
+        return _G.read()\\n\\
+    end,\\n\\
+    lines = function( self )\\n\\
+        return function()\\n\\
+            return _G.read()\\n\\
+        end\\n\\
+    end,\\n\\
+}\\n\\
+\\n\\
+local g_defaultOutput = {\\n\\
+    bFileHandle = true,\\n\\
+    bClosed = false,\\n\\
+    close = function( self )\\n\\
+    end,\\n\\
+    write = function( self, ... )\\n\\
+        local nLimit = select("#", ... )\\n\\
+        for n = 1, nLimit do\\n\\
+            _G.write( select( n, ... ) )\\n\\
+        end\\n\\
+    end,\\n\\
+    flush = function( self )\\n\\
+    end,\\n\\
+}\\n\\
+\\n\\
+local g_currentInput = g_defaultInput\\n\\
+local g_currentOutput = g_defaultOutput\\n\\
+\\n\\
+function close( _file )\\n\\
+    (_file or g_currentOutput):close()\\n\\
+end\\n\\
+\\n\\
+function flush()\\n\\
+    g_currentOutput:flush()\\n\\
+end\\n\\
+\\n\\
+function input( _arg )\\n\\
+    if _G.type( _arg ) == "string" then\\n\\
+        g_currentInput = open( _arg, "r" )\\n\\
+    elseif _G.type( _arg ) == "table" then\\n\\
+        g_currentInput = _arg\\n\\
+    elseif _G.type( _arg ) == "nil" then\\n\\
+        return g_currentInput\\n\\
+    else\\n\\
+        error( "bad argument #1 (expected string/table/nil, got " .. _G.type( _arg ) .. ")", 2 )\\n\\
+    end\\n\\
+end\\n\\
+\\n\\
+function lines( _sFileName )\\n\\
+    if _G.type( _sFileName ) ~= "string" then\\n\\
+        error( "bad argument #1 (expected string, got " .. _G.type( _sFileName ) .. ")", 2 )\\n\\
+    end\\n\\
+    if _sFileName then\\n\\
+        return open( _sFileName, "r" ):lines()\\n\\
+    else\\n\\
+        return g_currentInput:lines()\\n\\
+    end\\n\\
+end\\n\\
+\\n\\
+function open( _sPath, _sMode )\\n\\
+    if _G.type( _sPath ) ~= "string" then\\n\\
+        error( "bad argument #1 (expected string, got " .. _G.type( _sPath ) .. ")", 2 )\\n\\
+    end\\n\\
+    if _sMode ~= nil and _G.type( _sMode ) ~= "string" then\\n\\
+        error( "bad argument #2 (expected string, got " .. _G.type( _sMode ) .. ")", 2 )\\n\\
+    end\\n\\
+    local sMode = _sMode or "r"\\n\\
+    local file, err = fs.open( _sPath, sMode )\\n\\
+    if not file then\\n\\
+        return nil, err\\n\\
+    end\\n\\
+    \\n\\
+    if sMode == "r"then\\n\\
+        return {\\n\\
+            bFileHandle = true,\\n\\
+            bClosed = false,				\\n\\
+            close = function( self )\\n\\
+                file.close()\\n\\
+                self.bClosed = true\\n\\
+            end,\\n\\
+            read = function( self, _sFormat )\\n\\
+                local sFormat = _sFormat or "*l"\\n\\
+                if sFormat == "*l" then\\n\\
+                    return file.readLine()\\n\\
+                elseif sFormat == "*a" then\\n\\
+                    return file.readAll()\\n\\
+                elseif _G.type( sFormat ) == "number" then\\n\\
+                    return file.read( sFormat )\\n\\
+                else\\n\\
+                    error( "Unsupported format", 2 )\\n\\
+                end\\n\\
+                return nil\\n\\
+            end,\\n\\
+            lines = function( self )\\n\\
+                return function()\\n\\
+                    local sLine = file.readLine()\\n\\
+                    if sLine == nil then\\n\\
+                        file.close()\\n\\
+                        self.bClosed = true\\n\\
+                    end\\n\\
+                    return sLine\\n\\
+                end\\n\\
+            end,\\n\\
+        }\\n\\
+    elseif sMode == "w" or sMode == "a" then\\n\\
+        return {\\n\\
+            bFileHandle = true,\\n\\
+            bClosed = false,				\\n\\
+            close = function( self )\\n\\
+                file.close()\\n\\
+                self.bClosed = true\\n\\
+            end,\\n\\
+            write = function( self, ... )\\n\\
+                local nLimit = select("#", ... )\\n\\
+                for n = 1, nLimit do\\n\\
+                    file.write( select( n, ... ) )\\n\\
+                end\\n\\
+            end,\\n\\
+            flush = function( self )\\n\\
+                file.flush()\\n\\
+            end,\\n\\
+        }\\n\\
+    \\n\\
+    elseif sMode == "rb" then\\n\\
+        return {\\n\\
+            bFileHandle = true,\\n\\
+            bClosed = false,				\\n\\
+            close = function( self )\\n\\
+                file.close()\\n\\
+                self.bClosed = true\\n\\
+            end,\\n\\
+            read = function( self )\\n\\
+                return file.read()\\n\\
+            end,\\n\\
+        }\\n\\
+        \\n\\
+    elseif sMode == "wb" or sMode == "ab" then\\n\\
+        return {\\n\\
+            bFileHandle = true,\\n\\
+            bClosed = false,				\\n\\
+            close = function( self )\\n\\
+                file.close()\\n\\
+                self.bClosed = true\\n\\
+            end,\\n\\
+            write = function( self, ... )\\n\\
+                local nLimit = select("#", ... )\\n\\
+                for n = 1, nLimit do\\n\\
+                    file.write( select( n, ... ) )\\n\\
+                end\\n\\
+            end,\\n\\
+            flush = function( self )\\n\\
+                file.flush()\\n\\
+            end,\\n\\
+        }\\n\\
+    \\n\\
+    else\\n\\
+        file.close()\\n\\
+        error( "Unsupported mode", 2 )\\n\\
+        \\n\\
+    end\\n\\
+end\\n\\
+\\n\\
+function output( _arg )\\n\\
+    if _G.type( _arg ) == "string" then\\n\\
+        g_currentOutput = open( _arg, "w" )\\n\\
+    elseif _G.type( _arg ) == "table" then\\n\\
+        g_currentOutput = _arg\\n\\
+    elseif _G.type( _arg ) == "nil" then\\n\\
+        return g_currentOutput\\n\\
+    else\\n\\
+        error( "bad argument #1 (expected string/table/nil, got " .. _G.type( _arg ) .. ")", 2 )\\n\\
+    end\\n\\
+end\\n\\
+\\n\\
+function read( ... )\\n\\
+    return input():read( ... )\\n\\
+end\\n\\
+\\n\\
+function type( _handle )\\n\\
+    if _G.type( _handle ) == "table" and _handle.bFileHandle == true then\\n\\
+        if _handle.bClosed then\\n\\
+            return "closed file"\\n\\
+        else\\n\\
+            return "file"\\n\\
+        end\\n\\
+    end\\n\\
+    return nil\\n\\
+end\\n\\
+\\n\\
+function write( ... )\\n\\
+    return output():write( ... )\\n\\
+end\\n\\
 "},`)
     }
     fs.writeSync(out, " ".repeat((level - 1) * 4) + "}")
