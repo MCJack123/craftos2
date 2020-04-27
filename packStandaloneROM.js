9953542ee2a7ee01877a4d712b83dad1daefa156
MCJack123 <jackmacwindowslinux@gmail.com>
2020-04-27 00:27:10 -0400

    Fixed packStandaloneROM for Windows

--- packStandaloneROM.js
+++ packStandaloneROM.js
@@ -217,12 +217,12 @@ function writeDir(path, level) {
         if (fs.lstatSync(path + "/" + f).isDirectory()) writeDir(path + "/" + f, level + 1)
         else {
             console.log("Reading file " + path + "/" + f)
-            fs.writeSync(out, JSON.stringify(fs.readFileSync(path + "/" + f, "utf8")).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\\\n"))
+            fs.writeSync(out, JSON.stringify(fs.readFileSync(path + "/" + f, "utf8")).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\"\n\""))
         }
         fs.writeSync(out, "},\n")
     }
     if (path == "rom/apis") {
-        fs.writeSync(out, `${' '.repeat(level * 4)}{"${f}", ${JSON.stringify(ioapi).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\\\n")}},`)
+        fs.writeSync(out, `${' '.repeat(level * 4)}{"${f}", ${JSON.stringify(ioapi).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\"\n\"")}},`)
     }
     fs.writeSync(out, " ".repeat((level - 1) * 4) + "}")
 }
@@ -231,5 +231,5 @@ fs.writeSync(out, ");\n\nFileEntry standaloneDebug(")
 writeDir("debug", 1)
 fs.writeSync(out, ");\n\nstd::string standaloneBIOS = ")
 console.log("Reading BIOS")
-fs.writeSync(out, JSON.stringify(fs.readFileSync("bios.lua", "utf8")).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\\\n") + ";")
+fs.writeSync(out, JSON.stringify(fs.readFileSync("bios.lua", "utf8")).replace(/\\r\\n/g, "\\n").replace(/\\n/g, "\\n\"\n\"") + ";")
 fs.closeSync(out);
