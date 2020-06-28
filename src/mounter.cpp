ad98bddba250ccbc7fa3ad617bb8806fff3a2d74
MCJack123 <jackmacwindowslinux@gmail.com>
2020-06-28 02:44:55 -0400

    Fixed io.open not creating all parent directories

--- src/mounter.cpp
+++ src/mounter.cpp
@@ -284,11 +284,12 @@ int mounter_isReadOnly(lua_State *L) {
     return 0; // redundant
 }
 
+extern std::string fixpath_mkdir(Computer * comp, std::string path, bool md = true, std::string * mountPath = NULL);
+
 extern "C" FILE* mounter_fopen(lua_State *L, const char * filename, const char * mode) {
     if ((mode[0] != 'r' && mode[0] != 'w' && mode[0] != 'a') || (mode[1] != '\0' && !(mode[1] == 'b' && mode[2] == '\0'))) luaL_error(L, "Unsupported mode");
     if (get_comp(L)->files_open >= config.maximumFilesOpen) { errno = EMFILE; return NULL; }
-    std::string newpath = fixpath(get_comp(L), filename, mode[0] == 'r');
-    if (mode[0] == 'w' || mode[0] == 'a') createDirectory(newpath.substr(0, newpath.find_last_of(PATH_SEP)));
+    std::string newpath = mode[0] == 'r' ? fixpath(get_comp(L), lua_tostring(L, 1), true) : fixpath_mkdir(get_comp(L), lua_tostring(L, 1));
     FILE* retval = fopen(newpath.c_str(), mode);
     if (retval != NULL) get_comp(L)->files_open++;
     return retval;
