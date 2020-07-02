840dce183f401e60e5dd05df74297744baa82eb4
MCJack123 <jackmacwindowslinux@gmail.com>
2020-07-02 12:49:57 -0400

    Updated CC:T version

--- src/lib.hpp
+++ src/lib.hpp
@@ -21,7 +21,7 @@ extern "C" {
 }
 
 #define CRAFTOSPC_VERSION    "v2.3.4"
-#define CRAFTOSPC_CC_VERSION "1.89.1"
+#define CRAFTOSPC_CC_VERSION "1.89.2"
 #define CRAFTOSPC_INDEV      false
 
 struct Computer;
