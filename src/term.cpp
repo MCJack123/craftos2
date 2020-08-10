55d7b772ac428b5d39e8f1d88efc8fed5eb4e7e3
MCJack123 <jackmacwindowslinux@gmail.com>
2020-08-10 13:16:55 -0400

    Disabled fullscreen on WASM builds (closes #100)

--- src/term.cpp
+++ src/term.cpp
@@ -683,7 +683,9 @@ const char * termGetEvent(lua_State *L) {
             SDLTerminal * sdlterm = dynamic_cast<SDLTerminal*>(term);
             if (e.key.keysym.scancode == SDL_SCANCODE_F2 && (e.key.keysym.mod & ~(KMOD_CAPS | KMOD_NUM)) == 0 && sdlterm != NULL && !config.ignoreHotkeys) sdlterm->screenshot();
             else if (e.key.keysym.scancode == SDL_SCANCODE_F3 && (e.key.keysym.mod & ~(KMOD_CAPS | KMOD_NUM)) == 0 && sdlterm != NULL && !config.ignoreHotkeys) sdlterm->toggleRecording();
+#ifndef __EMSCRIPTEN__
             else if (e.key.keysym.scancode == SDL_SCANCODE_F11 && (e.key.keysym.mod & ~(KMOD_CAPS | KMOD_NUM)) == 0 && sdlterm != NULL && !config.ignoreHotkeys) sdlterm->toggleFullscreen();
+#endif
             else if (e.key.keysym.scancode == SDL_SCANCODE_F12 && (e.key.keysym.mod & ~(KMOD_CAPS | KMOD_NUM)) == 0 && sdlterm != NULL && !config.ignoreHotkeys) sdlterm->screenshot("clipboard");
             else if (((selectedRenderer == 0 || selectedRenderer == 5) ? e.key.keysym.scancode == SDL_SCANCODE_T : e.key.keysym.scancode == 20) && (e.key.keysym.mod & KMOD_CTRL)) {
                 if (computer->waitingForTerminate & 1) {
