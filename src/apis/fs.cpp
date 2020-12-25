496c369a574b851c4103eb6bedf39f405a98d2dc
MCJack123 <jackmacwindowslinux@gmail.com>
2020-12-25 13:52:59 -0500

    Fixed CI
    ...I should really test it first.

--- src/apis/fs.cpp
+++ src/apis/fs.cpp
@@ -348,7 +348,7 @@ static int fs_delete(lua_State *L) {
 static int fs_combine(lua_State *L) {
     lastCFunction = __func__;
     std::string basePath = luaL_checkstring(L, 1);
-    for (int i = 3; i <= lua_gettop(L); i++) basePath += "/" + std::string(luaL_checkstring(L, i));
+    for (int i = 2; i <= lua_gettop(L); i++) basePath += "/" + std::string(luaL_checkstring(L, i));
     lua_pushstring(L, astr(fixpath(get_comp(L), (basePath).c_str(), false, false)).c_str());
     return 1;
 }
