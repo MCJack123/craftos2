7f355ae23eb20477e50e3a7ce3ef8714ffd7d8b4
MCJack123 <jackmacwindowslinux@gmail.com>
2020-12-25 14:00:09 -0500

    Fixed CI

--- src/util.cpp
+++ src/util.cpp
@@ -136,7 +136,11 @@ path_t fixpath(Computer *comp, const char * path, bool exists, bool addExt, std:
     std::vector<std::string> elems = split(path, '/');
     std::list<std::string> pathc;
     for (const std::string& s : elems) {
-        if (s == "..") { if (pathc.empty()) return path_t(); else pathc.pop_back(); } else if (s != "." && !s.empty()) pathc.push_back(s);
+        if (s == "..") {
+            if (pathc.empty() && addExt) return path_t();
+            else if (pathc.empty()) pathc.push_back("..");
+            else pathc.pop_back();
+        } else if (s != "." && !s.empty()) pathc.push_back(s);
     }
     while (!pathc.empty() && pathc.front().empty()) pathc.pop_front();
     if (comp->isDebugger && addExt && pathc.size() == 1 && pathc.front() == "bios.lua")
