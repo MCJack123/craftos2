e88814759a1c1daf4c5cb700e9fea886cedd8af2
MCJack123 <jackmacwindowslinux@gmail.com>
2021-01-23 23:43:11 -0500

    v2.5.2: Fixed CI

--- src/apis/redstone.cpp
+++ src/apis/redstone.cpp
@@ -133,8 +133,8 @@ static int rs_setAnalogOutput(lua_State *L) {
     if (side == -1) return luaL_error(L, "bad argument #1 (unknown option %s)", sidestr.c_str());
     if (!lua_isnumber(L, 2)) return luaL_error(L, "bad argument #2 (number expected, got %s)", luaL_typename(L, 2));
     const lua_Number strength = lua_tonumber(L, 2);
-    if (isnan(strength)) return luaL_argerror(L, 2, "number expected, got nan");
-    if (isinf(strength)) return luaL_argerror(L, 2, "number expected, got inf");
+    if (isnan(strength)) return luaL_error(L, "bad argument #2 (number expected, got nan)");
+    if (isinf(strength)) return luaL_error(L, "bad argument #2 (number expected, got inf)");
     if (strength < 0 || strength > 15) return luaL_error(L, "Expected number in range 0-15");
     comp->redstoneOutputs[side] = (uint8_t)strength;
     return 0;
@@ -189,8 +189,8 @@ static int rs_setBundledOutput(lua_State *L) {
     if (side == -1) return luaL_error(L, "bad argument #1 (unknown option %s)", sidestr.c_str());
     if (!lua_isnumber(L, 2)) return luaL_error(L, "bad argument #2 (number expected, got %s)", luaL_typename(L, 2));
     const lua_Number strength = lua_tonumber(L, 2);
-    if (isnan(strength)) return luaL_argerror(L, 2, "number expected, got nan");
-    if (isinf(strength)) return luaL_argerror(L, 2, "number expected, got inf");
+    if (isnan(strength)) return luaL_error(L, "bad argument #2 (number expected, got nan)");
+    if (isinf(strength)) return luaL_error(L, "bad argument #2 (number expected, got inf)");
     if (strength < 0 || strength > 65535) return luaL_error(L, "Expected number in range 0-65535");
     comp->bundledRedstoneOutputs[side] = (uint16_t)strength;
     return 0;
@@ -211,8 +211,8 @@ static int rs_testBundledInput(lua_State *L) {
     if (side == -1) return luaL_error(L, "bad argument #1 (unknown option %s)", sidestr.c_str());
     if (!lua_isnumber(L, 2)) return luaL_error(L, "bad argument #2 (number expected, got %s)", luaL_typename(L, 2));
     const lua_Number mask = lua_tonumber(L, 2);
-    if (isnan(mask)) return luaL_argerror(L, 2, "number expected, got nan");
-    if (isinf(mask)) return luaL_argerror(L, 2, "number expected, got inf");
+    if (isnan(mask)) return luaL_error(L, "bad argument #2 (number expected, got nan)");
+    if (isinf(mask)) return luaL_error(L, "bad argument #2 (number expected, got inf)");
     if (mask < 0 || mask > 65535) return luaL_error(L, "Expected number in range 0-65535");
     lua_pushboolean(L, (comp->bundledRedstoneOutputs[side] & (uint16_t)mask) == (uint16_t)mask);
     return 1;
