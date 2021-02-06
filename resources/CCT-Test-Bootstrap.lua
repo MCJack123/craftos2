ea8982607877f1686dfae13f71f6d0b1a2ac911b
MCJack123 <jackmacwindowslinux@gmail.com>
2021-02-06 08:22:32 -0500

    Fixed CI?!?

--- resources/CCT-Test-Bootstrap.lua
+++ resources/CCT-Test-Bootstrap.lua
@@ -7,6 +7,8 @@ if ... == "debugger" then
     peripheral.call("left", "break")
 end
 for _,v in ipairs(fs.list("/")) do if not fs.isReadOnly(v) then fs.delete(v) end end
+_G._CCPC_FIRST_RUN = nil
+_G._CCPC_UPDATED_VERSION = nil
 local logfile = io.open("test-log.txt", "w")
 io.output(logfile)
 shell.run("/test-rom/mcfly /test-rom/spec")
