69f20daa63b6c07ccc06ff37de957407484abec5
MCJack123 <jackmacwindowslinux@gmail.com>
2021-03-13 23:14:52 -0500

    Bumped version

--- src/util.hpp
+++ src/util.hpp
@@ -24,9 +24,9 @@ extern "C" {
 #include <Computer.hpp>
 #include <Terminal.hpp>
 
-#define CRAFTOSPC_VERSION    "v2.5.3"
+#define CRAFTOSPC_VERSION    "v2.5.4"
 #define CRAFTOSPC_CC_VERSION "1.95.3"
-#define CRAFTOSPC_INDEV      false
+#define CRAFTOSPC_INDEV      true
 
 // for some reason Clang complains if this isn't present
 #ifdef __clang__
