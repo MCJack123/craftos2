35c16d41931280f03cb4227de1b0ecd9efdbdc51
MCJack123 <jackmacwindowslinux@gmail.com>
2021-04-19 22:12:16 -0400

    Fixed CI

--- src/util.hpp
+++ src/util.hpp
@@ -26,7 +26,7 @@ extern "C" {
 
 #define CRAFTOSPC_VERSION    "v2.6"
 #define CRAFTOSPC_CC_VERSION "2.0.0"
-#define CRAFTOSPC_INDEV      false
+#define CRAFTOSPC_INDEV      true
 
 // for some reason Clang complains if this isn't present
 #ifdef __clang__
