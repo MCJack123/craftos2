e3b200ce742b6c64bfa3f7623952ec9581525ea5
MCJack123 <jackmacwindowslinux@gmail.com>
2021-12-12 12:35:57 -0500

    Switched DFPWM implementation

--- src/peripheral/speaker.cpp
+++ src/peripheral/speaker.cpp
@@ -224,10 +224,17 @@ void setupForNextPlayback(float& speed, Mix_Chunk* chunk, int channel, bool loop
     }
 }
 
-// DFPWM transcoder from https://github.com/asiekierka/pixmess/blob/master/scraps/
+// DFPWM transcoder from https://github.com/ChenThread/dfpwm/blob/master/1a/
+
+#ifndef CONST_PREC
+#define CONST_PREC 10
+#endif
+#ifndef CONST_POSTFILT
+#define CONST_POSTFILT 140
+#endif
 
 // note, len denotes how many compressed bytes there are (uncompressed bytes / 8).
-static void au_compress(int *q, int *s, int *lt, int ri, int rd, int len, uint8_t *outbuf, int8_t *inbuf)
+static void au_compress(int *q, int *s, int *lt, int len, uint8_t *outbuf, int8_t *inbuf)
 {
 	int i,j;
 	uint8_t d = 0;
@@ -237,39 +244,41 @@ static void au_compress(int *q, int *s, int *lt, int ri, int rd, int len, uint8_
 		{
 			// get sample
 			int v = *(inbuf++);
-			
+
 			// set bit / target
 			int t = (v < *q || v == -128 ? -128 : 127);
 			d >>= 1;
 			if(t > 0)
 				d |= 0x80;
-			
+
 			// adjust charge
-			int nq = *q + ((*s * (t-*q) + 0x80)>>8);
+			int nq = *q + ((*s * (t-*q) + (1<<(CONST_PREC-1)))>>CONST_PREC);
 			if(nq == *q && nq != t)
 				nq += (t == 127 ? 1 : -1);
 			*q = nq;
-			
+
 			// adjust strength
-			int st = (t != *lt ? 0 : 255);
-			int sr = (t != *lt ? rd : ri);
-			int ns = *s + ((sr*(st-*s) + 0x80)>>8);
-			if(ns == *s && ns != st)
-				ns += (st == 255 ? 1 : -1);
+			int st = (t != *lt ? 0 : (1<<CONST_PREC)-1);
+			int ns = *s;
+			if(ns != st)
+				ns += (st != 0 ? 1 : -1);
+#if CONST_PREC > 8
+			if(ns < 1+(1<<(CONST_PREC-8))) ns = 1+(1<<(CONST_PREC-8));
+#endif
 			*s = ns;
-			
+
 			*lt = t;
-			
+
 			//fprintf(stderr, "%4i %4i %4i %4i\n", v, *q, *s, t);
 			//usleep(10000);
 		}
-		
+
 		// output bits
 		*(outbuf++) = d;
 	}
 }
 
-static void au_decompress(int *fq, int *q, int *s, int *lt, int fs, int ri, int rd, int len, int8_t *outbuf, uint8_t *inbuf)
+static void au_decompress(int *fq, int *q, int *s, int *lt, int fs, int len, int8_t *outbuf, uint8_t *inbuf)
 {
 	int i,j;
 	uint8_t d;
@@ -277,38 +286,40 @@ static void au_decompress(int *fq, int *q, int *s, int *lt, int fs, int ri, int 
 	{
 		// get bits
 		d = *(inbuf++);
-		
+
 		for(j = 0; j < 8; j++)
 		{
 			// set target
 			int t = ((d&1) ? 127 : -128);
 			d >>= 1;
-			
+
 			// adjust charge
-			int nq = *q + ((*s * (t-*q) + 0x80)>>8);
+			int nq = *q + ((*s * (t-*q) + (1<<(CONST_PREC-1)))>>CONST_PREC);
 			if(nq == *q && nq != t)
 				*q += (t == 127 ? 1 : -1);
 			int lq = *q;
 			*q = nq;
-			
+
 			// adjust strength
-			int st = (t != *lt ? 0 : 255);
-			int sr = (t != *lt ? rd : ri);
-			int ns = *s + ((sr*(st-*s) + 0x80)>>8);
-			if(ns == *s && ns != st)
-				ns += (st == 255 ? 1 : -1);
+			int st = (t != *lt ? 0 : (1<<CONST_PREC)-1);
+			int ns = *s;
+			if(ns != st)
+				ns += (st != 0 ? 1 : -1);
+#if CONST_PREC > 8
+			if(ns < 1+(1<<(CONST_PREC-8))) ns = 1+(1<<(CONST_PREC-8));
+#endif
 			*s = ns;
-			
+
 			// FILTER: perform antijerk
 			int ov = (t != *lt ? (nq+lq)>>1 : nq);
-			
+
 			// FILTER: perform LPF
 			*fq += ((fs*(ov-*fq) + 0x80)>>8);
 			ov = *fq;
-			
+
 			// output sample
 			*(outbuf++) = ov;
-			
+
 			*lt = t;
 		}
 	}
@@ -513,19 +524,14 @@ int speaker::playAudio(lua_State *L) {
             int q = 0;
             int s = 0;
             int lt = -128;
-            int ri = 7;
-            int rd = 20;
-            au_compress(&q, &s, &lt, ri, rd, len / 8 + (len % 8 != 0), encdata, data + 44);
+            au_compress(&q, &s, &lt, len / 8 + (len % 8 != 0), encdata, data + 44);
         }
         {
+            int fq = 0;
             int q = 0;
             int s = 0;
             int lt = -128;
-            int ri = 7;
-            int rd = 20;
-            int fq = 0;
-            int fs = 100;
-            au_decompress(&fq, &q, &s, &lt, fs, ri, rd, len / 8 + (len % 8 != 0), data + 44, encdata);
+            au_decompress(&fq, &q, &s, &lt, 100, len / 8 + (len % 8 != 0), data + 44, encdata);
         }
         delete[] encdata;
     }
