254e47bc638bb1bb990f8e6c2a862a837af719e8
MCJack123 <jackmacwindowslinux@gmail.com>
2023-01-22 15:48:19 -0500

    Fixed CI

--- src/apis/fs.cpp
+++ src/apis/fs.cpp
@@ -337,13 +337,13 @@ static int fs_move(lua_State *L) {
     bool isRoot = false;
     const path_t fromPath = fixpath(get_comp(L), str1, true, true, NULL, &isRoot);
     const path_t toPath = fixpath_mkdir(get_comp(L), str2);
-    if (isRoot) luaL_error(L, "Cannot move mount");
     if (fromPath.empty()) luaL_error(L, "No such file");
     if (toPath.empty()) err(L, 2, "Invalid path");
     if (std::regex_search((*fromPath.begin()).native(), pathregex("^\\d+:"))) err(L, 1, "Permission denied");
     if (std::regex_search((*toPath.begin()).native(), pathregex("^\\d+:"))) err(L, 2, "Permission denied");
     if (std::mismatch(toPath.begin(), toPath.end(), fromPath.begin(), fromPath.end()).second == fromPath.end()) 
         luaL_error(L, "Can't move a directory inside itself");
+    if (isRoot) luaL_error(L, "Cannot move mount");
     std::error_code e;
     if (fs::exists(toPath, e)) luaL_error(L, "File exists");
     e.clear();
